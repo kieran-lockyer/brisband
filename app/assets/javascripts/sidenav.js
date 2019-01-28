8cd99d292e8185529382e524e3bc7d47d903997d
thehicksivist <hicks.a.james@gmail.com>
2019-01-28 20:09:08 +1000

    burger menu functionality completed

--- app/assets/javascripts/sidenav.js
+++ app/assets/javascripts/sidenav.js
@@ -2,7 +2,7 @@
 function openNav() {
   console.log('openNav')
     // document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.transform = "translateX(100%)";
+    document.getElementById("mySidenav").style.transform = "translateX(0%)";
   }
   
   /* Set the width of the side navigation to 0 */
